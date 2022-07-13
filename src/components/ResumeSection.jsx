2e7d1904a8289be2a39ee362dc9732a3c6aa335c
balintkissdev <balintkissdev@gmail.com>
2022-07-13 22:21:49 +0200

    Change title formatting for ResumeSection

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -8,7 +8,7 @@ const ResumeSection = ({ id, title, children }) => {
         <>
             <section id={id} className={resumeSection + " p-3 p-lg-5 d-flex align-items-center"}>
                 <div class="w-100">
-                    {title && <h1 class="mb-5">{title}</h1>}
+                    {title && <h1 class="mb-3 display-4">{title}</h1>}
                     {children}
                 </div>
             </section>
