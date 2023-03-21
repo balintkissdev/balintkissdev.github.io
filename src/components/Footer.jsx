0d78acda061bf621ca48cf52361687848bfce23d
balintkissdev <balintkissdev@gmail.com>
2023-03-21 22:29:39 +0100

    Make sure year automatically updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 function Footer() {
     return (
         <footer className={`d-flex align-items-center justify-content-center`}>
-            <span className="py-3 text-muted">© 2022 Balint Kiss</span>
+            <span className="py-3 text-muted">&copy; {new Date().getFullYear()} Balint Kiss</span>
         </footer>
     )
 }
