ae77bdaa3fc992e50345f0787f1ebdbb3a61682d
balintkissdev <balintkissdev@gmail.com>
2023-09-04 23:22:50 +0200

    feat: Migrate from GatsbyJS to Astro

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
-import { StaticImage } from "gatsby-plugin-image";
+import profile from "../assets/cv_profile.jpg";
+
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
-import { Link, animateScroll} from "react-scroll"
+import { Link, animateScroll } from "react-scroll";
 
 import React, { useState } from "react";
 
@@ -27,7 +28,8 @@ function SocialIcon({ href, children }) {
 }
 
 export default function Sidebar() {
-  const hamburgerLine = "h-1 w-6 my-1 bg-[whitesmoke] transition ease transform duration-200"
+  const hamburgerLine =
+    "h-1 w-6 my-1 bg-[whitesmoke] transition ease transform duration-200";
 
   const [isOpen, setOpen] = useState(false);
 
@@ -43,69 +45,69 @@ export default function Sidebar() {
 
   return (
     <>
-    <button
-      className={`fixed flex flex-col justify-center items-center top-4 right-6 h-12
+      <button
+        className={`fixed flex flex-col justify-center items-center top-4 right-6 h-12
       w-12 p-2 cursor-pointer rounded-lg bg-[#383837] lg:hidden`}
-      onClick={toggleIsOpen}
-    >
-      <span
-        className={`${hamburgerLine} ${isOpen && "rotate-45 translate-y-[0.65rem]"}`}
-      ></span>
-      <span
-        className={`${hamburgerLine} ${isOpen && "opacity-0"}`}
-      ></span>
-      <span
-        className={`${hamburgerLine} ${isOpen && "-rotate-45 -translate-y-[0.65rem]"}`}
-      ></span>
-    </button>
-    <nav className={`
+        onClick={toggleIsOpen}
+      >
+        <span
+          className={`${hamburgerLine} ${
+            isOpen && "rotate-45 translate-y-[0.65rem]"
+          }`}
+        ></span>
+        <span className={`${hamburgerLine} ${isOpen && "opacity-0"}`}></span>
+        <span
+          className={`${hamburgerLine} ${
+            isOpen && "-rotate-45 -translate-y-[0.65rem]"
+          }`}
+        ></span>
+      </button>
+      <nav
+        className={`
         fixed lg:flex flex-col justify-center max-w-screen-sm lg:max-w-[16rem]
         h-full bg-[#383837] text-[whitesmoke] px-6 gap-8 transition lg:transition-none duration-200 ease-in z-10
         ${isOpen ? "translate-x0" : "-translate-x-96 lg:translate-x-0"}
-      `}>
-      <div className="flex justify-center">
-        {/* HACK: gatsby-image-wrapper overrides with "inline-block" */}
-        <div className="hidden lg:inline-block">
-          <a
-            href="#top"
-            className="transition ease-in-out hover:brightness-110 duration-300"
-          >
-            <StaticImage
-              className="rounded-full"
-              src="../images/cv_profile.jpg"
-            />
-          </a>
+      `}
+      >
+        <div className="flex justify-center">
+          <div className="hidden lg:inline-block">
+            <a
+              href="#top"
+              className="transition ease-in-out hover:brightness-110 duration-300"
+            >
+              <img className="rounded-full" src={profile.src} />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center pt-4 gap-4">
-        <SocialIcon href="https://linkedin.com/in/balintkissdev">
-          <FaLinkedin size={26} />
-        </SocialIcon>
-        <SocialIcon href="https://github.com/balintkissdev">
-          <FaGithub size={26} />
-        </SocialIcon>
-        <SocialIcon href="mailto:balintkissdev@gmail.com">
-          <FaEnvelope size={26} />
-        </SocialIcon>
-      </div>
-      <ul className="mx-2 uppercase font-bold tracking-wider">
-        {sideBarLinks.map((link) => {
-          return (
-            <li className="my-8" key={link.href}>
-              <Link
-                to={link.href}
-                smooth={true}
-                duration={500}
-                className="transition ease-in-out hover:text-[burlywood] duration-300"
-                onClick={() => setOpen(false)}
-              >
-                {link.name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+        <div className="flex justify-center pt-4 gap-4">
+          <SocialIcon href="https://linkedin.com/in/balintkissdev">
+            <FaLinkedin size={26} />
+          </SocialIcon>
+          <SocialIcon href="https://github.com/balintkissdev">
+            <FaGithub size={26} />
+          </SocialIcon>
+          <SocialIcon href="mailto:balintkissdev@gmail.com">
+            <FaEnvelope size={26} />
+          </SocialIcon>
+        </div>
+        <ul className="mx-2 uppercase font-bold tracking-wider">
+          {sideBarLinks.map((link) => {
+            return (
+              <li className="my-8" key={link.href}>
+                <Link
+                  to={link.href}
+                  smooth={true}
+                  duration={500}
+                  className="transition ease-in-out hover:text-[burlywood] duration-300"
+                  onClick={() => setOpen(false)}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
     fontSize: {
