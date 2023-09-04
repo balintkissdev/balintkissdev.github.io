c7dcc52ce85a4ec0624ff33c3879a7a5eb423a7c
balintkissdev <balintkissdev@gmail.com>
2023-09-04 23:22:50 +0200

    feat: Migrate from GatsbyJS to Astro

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-export default function Footer() {
-    return (
-        <footer className="flex justify-center py-4 text-gray-500">
-            <p>&copy; {new Date().getFullYear()} Balint Kiss</p>
-        </footer>
-    )
-}
